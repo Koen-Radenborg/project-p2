4ed46b12012d8044d411d6a8d6a557e2542bbc31
fondse <damian.jf2209@gmail.com>
2024-01-23 10:46:29 +0100

    ...

---
+++ damian.js
@@ -0,0 +1,47 @@
+// dit geeft de ID's aan voor de html elementen
+let block = document.getElementById("block");
+let hole = document.getElementById("hole");
+let character = document.getElementById("character");
+
+// dit houd de score bij van de game
+let jumping = 0;
+let counter = 0;
+
+// dit zorgt ervoor dat er op een willekeurige positie een gat komt
+hole.addEventListener('animationiteration', () => {
+    let random = -((Math.random()*300)+150);
+    hole.style.top = random + "px";
+    counter++;
+});
+//voert een functie uit en verplaats het rondje naar beneden als je niet springt 
+setInterval(function(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0){
+        character.style.top = (characterTop+3)+"px";
+    }
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    let cTop = -(500-characterTop);
+    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+        alert("Game over. Score: "+(counter-1));
+        character.style.top = 100 + "px";
+        counter=0;
+    }
+},10);
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    let jumpInterval = setInterval(function(){
+        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-5)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+}
