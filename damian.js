3197278509ef43344e645f1992442e17f9a2ffbb
fondse <damian.jf2209@gmail.com>
2024-01-25 09:18:55 +0100

    .

--- damian.js
+++ damian.js
@@ -1,30 +1,30 @@
 // dit geeft de ID's aan voor de html elementen
-let block = document.getElementById("block");
-let hole = document.getElementById("hole");
-let character = document.getElementById("character");
+let blockdamian = document.getElementById("blockdamian");
+let holedamian = document.getElementById("holedamian");
+let characterdamian = document.getElementById("characterdamian");
 
 // dit houd de score bij van de game
 let jumping = 0;
 let counter = 0;
 
 // dit zorgt ervoor dat er op een willekeurige positie een gat komt
-hole.addEventListener('animationiteration', () => {
+holedamian.addEventListener('animationiteration', () => {
     let random = -((Math.random()*300)+150);
-    hole.style.top = random + "px";
+    holedamian.style.top = random + "px";
     counter++;
 });
 //voert een functie uit en verplaats het rondje naar beneden als je niet springt 
 setInterval(function(){
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let characterTop = parseInt(window.getComputedStyle(characterdamian).getPropertyValue("top"));
     if(jumping==0){
-        character.style.top = (characterTop+3)+"px";
+        characterdamian.style.top = (characterTop+3)+"px";
     }
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(blockdamian).getPropertyValue("left"));
+    let holeTop = parseInt(window.getComputedStyle(holedamian).getPropertyValue("top"));
     let cTop = -(500-characterTop);
     if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
         alert("Game over. Score: "+(counter-1));
-        character.style.top = 100 + "px";
+        characterdamian.style.top = 100 + "px";
         counter=0;
     }
 },10);
@@ -35,7 +35,7 @@ function jump(){
     let jumpInterval = setInterval(function(){
         let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
         if((characterTop>6)&&(jumpCount<15)){
-            character.style.top = (characterTop-5)+"px";
+            characterdamain.style.top = (characterTop-5)+"px";
         }
         if(jumpCount>20){
             clearInterval(jumpInterval);
